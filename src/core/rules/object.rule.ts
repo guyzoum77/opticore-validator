41b7bf356c4c25542f0b0e65ac7e4457f4714d57
guyzoum77 <guyzoum7700@mail.com>
2025-01-30 00:17:10 +0000

    finishing the rule setting up

--- src/core/rules/object.rule.ts
+++ src/core/rules/object.rule.ts
@@ -1,4 +1,4 @@
 export function objectRule(value: any): boolean {
-    return typeof value === "objectRule" && value !== null && !Array.isArray(value);
+    return typeof value === "object" && value !== null && !Array.isArray(value);
 }
   
